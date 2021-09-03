0612e9af074360b3d7c8b5b84902974518522f05
Zion <490625185@qq.com>
2021-09-03 20:43:27 +0800

    一些修改
    输出为jpg图象和加入Openmp多线程

--- color.h
+++ color.h
@@ -5,7 +5,9 @@
 #include "rtweekend.h"
 #include <iostream>
 
-void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+
+
+void write_color(/*std::ostream& out, */color pixel_color, int samples_per_pixel, unsigned char* data, int x, int y, int i, int j) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
@@ -16,10 +18,14 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     g = sqrt(scale * g);
     b = sqrt(scale * b);
 
+    data[(y - j - 1) * x * 3 + i * 3] = unsigned char(256 * clamp(r, 0.0, 0.999));
+    data[(y - j - 1) * x * 3 + i * 3 + 1] = unsigned char(256 * clamp(g, 0.0, 0.999));
+    data[(y - j - 1) * x * 3 + i * 3 + 2] = unsigned char(256 * clamp(b, 0.0, 0.999));
+
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
-        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
-        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+    //out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+    //    << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+    //    << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 #include "box.h"
 #include "bvh.h"
 #include "constant_medium.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#include <omp.h>
 
 
 #include <iostream>
@@ -251,15 +254,16 @@ hittable_list final_scene() {
     return objects;
 }
 
+
 int main() {
 
     // Image
 
-     auto aspect_ratio = 16.0 / 9.0;
-     int image_width = 400;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 800;
     
-     int samples_per_pixel = 100;
-     int max_depth = 50;
+    int samples_per_pixel = 1000;
+    int max_depth = 50;
 
     // World
 
@@ -346,7 +350,7 @@ int main() {
             world = final_scene();
             aspect_ratio = 1.0;
             image_width = 800;
-            samples_per_pixel = 10000;
+            samples_per_pixel = 1000;
             background = color(0,0,0);
             lookfrom = point3(478, 278, -600);
             lookat = point3(278, 278, 0);
@@ -378,21 +382,29 @@ int main() {
     // Render
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-    
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-       
+
+    unsigned char* data = new unsigned char[image_width * image_height * 3];
+
+
+
+
+    #pragma omp parallel for
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' <<'\n' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
             for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, background, world, max_depth);
             }
-            write_color(std::cout, pixel_color, samples_per_pixel);
+            write_color(pixel_color, samples_per_pixel, data, image_width, image_height, i, j);
         }
     }
+    stbi_write_jpg("D://N2Ray//N2Ray//Debug//output.jpg", image_width, image_height, 3, data, 100);
+    delete[] data;
+
 
     std::cerr << "\nDone.\n";
 }
